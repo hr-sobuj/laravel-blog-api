1b2b12ab7ec4e54610d907c8bfff426ea77a7105
hr-sobuj <sobujhd@gmail.com>
2024-04-10 10:57:09 +0600

    santum added

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,18 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Route;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('category',CategoryController::class);
     Route::apiResource('post',PostController::class);
 });
+
